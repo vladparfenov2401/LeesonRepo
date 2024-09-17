45f90cab19012d0936f44094ac70c85208d7b438
Vlad Parfenov <parfenov.v@gigwork.ru>
2024-09-17 09:36:30 +0300

    qqq

--- tests/telega.py
+++ tests/telega.py
@@ -1,17 +1,24 @@
-import asyncio
-from telegram import Bot
+import requests
+import unittest
 
-# Замените на ваш токен бота
-TELEGRAM_BOT_TOKEN = '7118565111:AAFdA8D59Mw_lYYkB2fSZyprzsVkYEM0-f0'
+class APITest(unittest.TestCase):
+    def setUp(self):
+}
+        self.data = {
+            "currentPeriod": {"start": "2024-09-07", "end": "2024-09-13"},
+            "subjects": [],
+            "brands": [],
+            "nms": [],
+            "tagIds": [],
+            "skipDeletedNm": False,
+            "orderBy": {"field": "orders", "mode": "desc"},
+            "prevPeriod": {"start": "2024-08-31", "end": "2024-09-06"}
+        }
 
-async def main():
-    bot = Bot(token=TELEGRAM_BOT_TOKEN)
+    def test_sales_funnel_report(self):
+        response = requests.post(self.url, headers=self.headers, json=self.data)
+        self.assertEqual(response.status_code, 200, f"Expected status code 200, but got {response.status_code}")
+        # Можно добавить дополнительные проверки здесь, если нужно
 
-    # Получаем обновления
-    updates = await bot.get_updates()
-
-    for update in updates:
-        print(update.message.chat.id)
-
-# Запуск асинхронной функции
-asyncio.run(main())
+if __name__ == '__main__':
+    unittest.main()
